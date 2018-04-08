aa98ef710eec2cdc14243ee9f99b88318c0f9026
廖冠霖 <leoliao1217@hotmail.com>
2018-04-08 23:26:49 +0800

    hello world

---
+++ src/Prototype.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Prototype { 
+    public static void main(String[] args) { 
+        JFrame category = new JFrame();
+        category.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        category.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        category.getContentPane().setLayout(new GridLayout(4,2));
+        Button button1 = new Button("廖冠霖");
+        Button button2 = new Button("呂舒喬");
+        Button button3 = new Button("徐禎廷");
+        Button button4 = new Button("孫偉碩");
+        Button button5 = new Button("凌欣恩");
+        Button button6 = new Button("黃律鈞");
+        Button button7 = new Button("廖秋旻");
+                
+        
+        category.add(button1);
+        category.add(button2);
+        category.add(button3);
+        category.add(button4);
+        category.add(button5);
+        category.add(button6);
+        category.add(button7);
+        
+        button1.addActionListener(new videolist1());
+        button2.addActionListener(new videolist2());
+        button3.addActionListener(new videolist3());
+        button4.addActionListener(new videolist4());
+        button5.addActionListener(new videolist5());
+        button6.addActionListener(new videolist6());
+        button7.addActionListener(new videolist7());
+
+        
+        
+        category.setVisible(true);
+    }
+    
+    
+}
---
+++ src/videolist1.java
@@ -0,0 +1,27 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+public class videolist1 implements ActionListener {
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		
+		// TODO Auto-generated method stub
+		JFrame videolist1 = new JFrame("廖冠霖");
+		videolist1.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		videolist1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblAngelaSorry = new JLabel("ANGELA SORRY");
+		videolist1.getContentPane().add(lblAngelaSorry, BorderLayout.CENTER);
+		
+		videolist1.setVisible(true);  
+	}
+
+}
