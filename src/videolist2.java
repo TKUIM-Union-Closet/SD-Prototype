6764a91d63bf331a61e506709fa0e873975b54ad
廖冠霖 <leoliao1217@hotmail.com>
2018-04-10 00:20:28 +0800

    知道如何放youtube但是還不會加入frame裡面

--- src/videolist2.java
+++ src/videolist2.java
@@ -1,12 +1,35 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class videolist2 implements ActionListener {
+import javax.swing.JFrame;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+
+public class videolist2 implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
 
+	    
+	    
 	}
+	
+//	public static void main(String[] args) { launch(args); }
+//
+//	  @Override public void start(Stage stage) throws Exception {
+//	    WebView webview = new WebView();
+//	    webview.getEngine().load(
+//	      "https://www.youtube.com/embed/TZDxz0ZpLpE?autoplay=1"
+//	    );
+//	    webview.setPrefSize(1920, 1080);
+//
+//	    stage.setScene(new Scene(webview));
+//	    stage.show();
+//	  }   
 
 }
+
+
